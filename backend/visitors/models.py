a53dbd6c15e592e1e418f3007e3aa04e99febb61
Said <baraoudev@gmail.com>
2026-04-09 19:32:52 +0200

    add visitor model

--- backend/visitors/models.py
+++ backend/visitors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Visitor(models.Model):
+  first_name = models.Charfield(max_length=50)
+  last_name = models.Charfield(max_length=50)
+  email =  models.EmailField()
+  company = models.Charfield(max_length=100, blank=True, null= True)
+  check_in_time = models.DateTimeField(auto_now_add=True)
+  is_stil_on_site = models.BooleanField(default=True)
+
+  def __str__(self):
+    return f"{self.first_name} {self.last_name}"
