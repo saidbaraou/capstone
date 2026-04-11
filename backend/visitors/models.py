621800805d30896356351c67d23057d0e0719869
Said <baraoudev@gmail.com>
2026-04-11 23:09:57 +0200

    add djangorestframework via pip freeze to requirements.txt, change Charfield to CharField in Visitor Model and make migrations/migrate

--- backend/visitors/models.py
+++ backend/visitors/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Visitor(models.Model):
-  first_name = models.Charfield(max_length=50)
-  last_name = models.Charfield(max_length=50)
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
   email =  models.EmailField()
-  company = models.Charfield(max_length=100, blank=True, null= True)
+  company = models.CharField(max_length=100, blank=True, null= True)
   check_in_time = models.DateTimeField(auto_now_add=True)
   is_stil_on_site = models.BooleanField(default=True)
 
