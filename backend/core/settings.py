d9dcbb5d17402653fee06717fc14afc0d61a874d
Said <baraoudev@gmail.com>
2026-04-13 22:30:29 +0200

    add allowed requests from react vite in settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -125,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.1:5173",
+]
