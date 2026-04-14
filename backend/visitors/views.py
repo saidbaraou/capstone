99c2a06e8a6aeeb4c3e5ec81a87310fab0b47aae
Said <baraoudev@gmail.com>
2026-04-14 18:41:12 +0200

    create automatic outer for the API, and the route to that router

--- backend/visitors/views.py
+++ backend/visitors/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Visitor
+from .serializer import VisitorSerializer
 
-# Create your views here.
+class VisitorViewSet(viewsets.ModelViewSet):
+    queryset = Visitor.objects.all()
+    serializer_class = VisitorSerializer
+
