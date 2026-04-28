fe4ac3d33d4b1b22fed8de003d4c5081a9f64265
Said <baraoudev@gmail.com>
2026-04-28 20:22:58 +0200

    Correct still in is_still_on_site Visitor Object property

--- backend/visitors/models.py
+++ backend/visitors/models.py
@@ -7,7 +7,7 @@ class Visitor(models.Model):
   email =  models.EmailField()
   company = models.CharField(max_length=100, blank=True, null= True)
   check_in_time = models.DateTimeField(auto_now_add=True)
-  is_stil_on_site = models.BooleanField(default=True)
+  is_still_on_site = models.BooleanField(default=True)
 
   def __str__(self):
     return f"{self.first_name} {self.last_name}"
