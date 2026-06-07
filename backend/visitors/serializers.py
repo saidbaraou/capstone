2e02ac7a66666f8ada195ff31a978450821cfdc2
Said <baraoudev@gmail.com>
2026-06-07 21:19:59 +0200

    implement VisitSerializer in order to translate database django objects to JSON and vice-versa

--- backend/visitors/serializers.py
+++ backend/visitors/serializers.py
@@ -5,4 +5,29 @@ class VisitorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Visitor
         fields = '__all__' # This will include all fields of the Visitor model in the serialized output
-        
+
+class VisitSerializer(serializers.ModelSerializer):
+    # display host's full name instead of just the ID
+    host_name = serializers.ReadOnlyField(source='host.get_full_name')
+    # Optionally include visitor's email in the serialized output
+    visitor_email = serializers.Readonlyfield(source='host.email')
+
+    class Meta:
+        model = Visit
+        fields = [
+            'id',
+            'visitor_full_name',
+            'visitor_email',
+            'visitor_company',
+            'host',
+            'host_name',
+            'planned_arrival',
+            'check_in_time',
+            'check_out_time',
+            'status',
+            'purpose_of_visit',
+            'safety_instructions_signed',
+            'nda_accepted',
+        ]
+        # ID and arrival/departure times are managed by the system, so they should be read-only
+        read_only_fields = ['id', 'check_in_time', 'check_out_time']
